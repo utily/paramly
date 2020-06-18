0bc306c9392803476c88d03d32343a02383f106f
Ishai Mika <twiggynao@gmail.com>
2020-06-18 11:26:15 +0200

    Added test. Fixed flag related bugs. (#2)
    Co-authored-by: Ishai Mika <ishai.mika@payfunc.com>

---
+++ index.spec.ts
@@ -0,0 +1,28 @@
+import * as paramly from "./index"
+
+describe("paramly", () => {
+	const application = new paramly.Application("Test", "test", "13.37", { "-t": 1 }, async f => f)
+	it("version", async () => {
+		let output = ""
+		console.log = jest.fn(inputs => output += inputs)
+		expect(await application.execute(["version"])).toEqual(true)
+		expect(output).toBe("Test\nversion: 13.37\n")
+	})
+	it("help", async () => {
+		let output = ""
+		console.log = jest.fn(inputs => output += inputs)
+		expect(await application.execute(["help"])).toEqual(true)
+		expect(output).toBe(`
+Test
+
+UsageTo get started, set a server.
+The server with the name default is used by default when no --server flag is used.
+
+ test help <module>	Get help on module
+
+Modules:
+help            Shows help.
+version         Shows version.
+`)
+	})
+})
